3deeec844676ca367e5084623c561ab1f8f8abf2
Maksim Rozhkov <kermit@mail.ru>
2019-10-26 12:46:37 +0300

    Изменения в реализации сетевой инфраструктуры

--- MyQmlProxyClass.cpp
+++ MyQmlProxyClass.cpp
@@ -1,4 +1,6 @@
 #include <QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include "myhttpservice.h"
 #include "myqmlproxyclass.h"
 
@@ -31,12 +33,28 @@ void MyQmlProxyClass::cppOnButtonClicked() {
 
     // Добавляем ещё несколько обработчиков событий, но уже связанных
     // с QNetworkReply, а не с QNetworkAccessManager
-    /*
-    connect(networkReply, &QNetworkReply::error, this, &MyClass::networkReplyError);
-    connect(networkReply, &QNetworkReply::QOverload<QNetworkReply::NetworkError>::of(&QNetworkReplu::error, this, &MyClass::networkReplyError);
-    connect(networkReply, &QNetworkReply::finished, this, &MyClass::requestFinised);
-    connect(networkReply, &QNetworkReply::readyRead, this, &MyClass::readyRead);
-    */
+
+    // Обработка ошибок подключения для различных ситуаций
+    // connect(networkReply, &QNetworkReply::error, this, &MyClass::networkReplyError);
+    //connect(networkReply, &QNetworkReply::QOverload<QNetworkReply::NetworkError>::of(&QNetworkReply::error,
+    //    this, &MyClass::networkReplyError);
+
+    // В теории, необходимо обрабатывать загруженные документы в слоте readyRead,
+    // но на практике это можно делать и в слоте replyFinished
+    // connect(networkReply, &QNetworkReply::readyRead, this, &MyClass::readyRead);
+
+    // Сигнал QNetworkReply::finished может быть использован для обработки
+    // полученных результатов
+    // TODO: разобраться, в чём разница между QNetworkAccessManager::finished
+    // и QNetworkReply::finished.
+    // Для примера может быть использовано лямбда-выражение, а не отдельный метод
+    QObject::connect(networkReply, &QNetworkReply::finished, [=]()
+    {
+        qDebug() << "We have got some headers:";
+
+        QList<QPair<QByteArray, QByteArray>> responses = networkReply->rawHeaderPairs();
+        qDebug() << responses;
+    });
 }
 
 void MyQmlProxyClass::replyFinished(QNetworkReply *reply) {
@@ -48,10 +66,41 @@ void MyQmlProxyClass::replyFinished(QNetworkReply *reply) {
     } else {
 
         qDebug() << "The data was received";
-        //...reply->rawHeaderList();
-    }
 
-    // Указывем на необходимость последующего удаления памяти,
-    // зарезервированной для хранения http-запросов
-    reply->deleteLater();
+        // Указываем на необходимость последующего удаления памяти,
+        // зарезервированной для хранения http-запросов
+        reply->deleteLater();
+
+        // Обрабатываем ответ сервера
+        if(!reply->error()) {
+
+            // TODO: Здесь имеет смысл обработкать HTTP Status Codes,
+            // например, так:
+            //
+            // if (reply->error() == QNetworkReply::OperationCanceledError) {}
+            // else if (reply->error() > 400 && reply->error() < 500) {}
+            // else if (reply->error() == QNetworkReply::AuthenticationRequiredError) {}
+            // else if (reply->error() == QNetworkReply::HostNotFoundError || reply->error() == QNetworkReply::ConnectionRefusedError)
+
+            QByteArray receivedData = reply->readAll();
+            if (receivedData.isEmpty())
+            {
+                qDebug() << "Empty JSON data";
+                return;
+            }
+
+            QJsonParseError e;
+            QJsonDocument d = QJsonDocument::fromJson(receivedData, &e);
+            if (!d.isNull()) {
+                if (d.isEmpty())
+                    qDebug() << "Получены пустые данные";
+
+                if (d.isObject())
+                    qDebug() << "Получен объект";
+
+                if (d.isArray())
+                    qDebug() << "Получен объект";
+            }
+        }
+    }
 }
